4fff340fd76547ddc5e49839c0e8d7d34b2f81a9
grudsside <uhalovgrigorij40731@gmail.com>
2026-08-02 13:42:24 +0300

    Allow pending task status actions

---
+++ backend/migrations/004_telegram_task_status_actions.sql
@@ -0,0 +1,6 @@
+alter table telegram_pending_actions
+    drop constraint if exists telegram_pending_actions_action_type_check;
+
+alter table telegram_pending_actions
+    add constraint telegram_pending_actions_action_type_check
+    check (action_type in ('create_task', 'update_task_status'));
